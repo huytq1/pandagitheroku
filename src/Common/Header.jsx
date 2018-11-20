1937c2fad2a4f1a7a570c9bbd3cdf8fd7d76988b
huytran84 <htran84@csc.com>
2018-11-20 13:12:44 +0700

    change content header

--- src/Common/Header.jsx
+++ src/Common/Header.jsx
@@ -1,9 +1,21 @@
 import React, { Component } from "react";
-
+import { NavLink } from "react-router-dom";
 class Header extends Component {
   state = {};
   render() {
-    return <div>Header</div>;
+    return (
+      <div className="text-center">
+        <nav className="navbar navbar-default">
+          <NavLink to="/" activeClassName="active">
+            Home
+          </NavLink>
+          {" | "}
+          <NavLink to="library" activeClassName="active">
+            Library
+          </NavLink>
+        </nav>
+      </div>
+    );
   }
 }
 
