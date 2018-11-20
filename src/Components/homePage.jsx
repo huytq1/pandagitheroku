a6579ce4fa850bfb11d75f438c125dd66d980309
huytran84 <htran84@csc.com>
2018-11-20 13:15:08 +0700

    add library page

--- src/Components/homePage.jsx
+++ src/Components/homePage.jsx
@@ -1,9 +1,20 @@
 import React, { Component } from "react";
-
+import { NavLink } from "react-router-dom";
 class HomePage extends Component {
   state = {};
   render() {
-    return <div> Home Page</div>;
+    return (
+      <div className="jumbotron center">
+        <h1 className="lead">
+          Welcome to Media Library built with React, Redux, and Redux-saga{" "}
+        </h1>
+        <div>
+          <NavLink to="library">
+            <button className="btn btn-lg btn-primary"> Visit Library</button>
+          </NavLink>
+        </div>
+      </div>
+    );
   }
 }
 
---
+++ src/Components/libraryPage.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class LibraryPage extends Component {
+  state = {};
+  render() {
+    return <div>Library Page</div>;
+  }
+}
+
+export default LibraryPage;
