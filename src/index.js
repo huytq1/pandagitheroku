18290db432c4e492d98fe49ce151993a8cdf82cd
huytran84 <htran84@csc.com>
2018-11-21 11:05:04 +0700

    fix css

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import App from "./Containers/App";
 import * as serviceWorker from "./serviceWorker";
 import { Provider } from "react-redux";
 import configureStore from "./store/configureStore";
+import "./styles/styles.css";
 const store = configureStore();
 
 ReactDOM.render(
